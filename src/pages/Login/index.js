99ce869e8a7daccff59b3f14f561f668d40e5086
Marco Antonio de Souza Filho <marco-antonio-souza@outlook.com>
2021-07-21 17:34:32 -0300

    Adding screen of login.

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import './styles.css';
+import Cadeado from '../../assests/Cadeado.png';
 
 export default function Login() {
     return (
-        <h1>Welcome</h1>
+        <div className="login container">
+            <section className="form">
+                <form action="">
+                    <h1>Acess your Account</h1>
+                    <input placeholder="Username" />
+                    <input type="password" placeholder="Password" />
+
+                    <button type="submit">Login</button>
+                </form>
+            </section>
+            <img src={Cadeado} alt="Login"/>
+        </div>       
     );
 }
