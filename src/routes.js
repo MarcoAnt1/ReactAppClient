d7747366abdb7c09fb9f511cf46e4dec1fb3c05a
Marco Antonio de Souza Filho <marco-antonio-souza@outlook.com>
2021-07-22 12:38:52 -0300

    Adding route for book.

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Login from './pages/Login';
+import Book from './pages/Book';
 
 export default function Routes() {
     return (
         <BrowserRouter>
             <Switch>
-                <Route pat="/" component={Login}/>
+                <Route path="/" exact component={Login}/>
+                <Route path="/book" component={Book}/>
             </Switch>
         </BrowserRouter>
     );
