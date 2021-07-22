4624e66e95d5fa088b0c6235d5f77ac0d29dcbbf
Marco Antonio de Souza Filho <marco-antonio-souza@outlook.com>
2021-07-22 17:27:43 -0300

    Calling api on login.

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { useHistory } from 'react-router-dom'
+
+import api from '../../services/api'
 
 import './styles.css';
 
@@ -6,14 +9,38 @@ import logo from '../../assests/logo.svg';
 import padlock from '../../assests/padlock.png';
 
 export default function Login() {
+    const [userName, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+
+    const history = useHistory();
+
+    async function login(e) {
+        e.preventDefault();
+
+        const data = { userName, password }
+
+        try {
+            const response = await api.post('v1/api/auth/signin', data);
+
+            localStorage.setItem('userName', userName);
+            localStorage.setItem('accessToken', response.data.accessToken);
+            localStorage.setItem('refreshToken', response.data.refreshToken);
+
+            history.push('/books')
+        } catch (error) {
+            alert('Login failed! Try again!')
+        }
+
+    }
+
     return (
         <div className="global-container">
             <section className="form">
                 <img src={logo} alt="Login"/>
-                <form action="">
+                <form onSubmit={login}>
                     <h1>Acess your Account</h1>
-                    <input placeholder="Username" />
-                    <input type="password" placeholder="Password" />
+                    <input placeholder="Username" value={userName} onChange={e => setUserName(e.target.value)} />
+                    <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)}  />
 
                     <button className="button" type="submit">Login</button>
                 </form>
