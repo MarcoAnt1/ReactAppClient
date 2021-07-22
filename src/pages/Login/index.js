fd758fc21c26bdfa1fc23f2856691dee82aad5da
Marco Antonio de Souza Filho <marco-antonio-souza@outlook.com>
2021-07-22 12:03:12 -0300

    Adjusts login.

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import './styles.css';
-import Cadeado from '../../assests/Cadeado.png';
+
+import logo from '../../assests/logo.png';
+import login from '../../assests/login.png';
 
 export default function Login() {
     return (
         <div className="login container">
             <section className="form">
+                <img src={logo} alt="Login"/>
                 <form action="">
                     <h1>Acess your Account</h1>
                     <input placeholder="Username" />
                     <input type="password" placeholder="Password" />
 
-                    <button type="submit">Login</button>
+                    <button className="button" type="submit">Login</button>
                 </form>
             </section>
-            <img src={Cadeado} alt="Login"/>
+            <img src={login} alt="Login"/>
         </div>       
     );
 }
